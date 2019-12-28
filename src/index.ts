c6bfa88b14bb64c3d38b1619f4b427351253cb6b
Chris Atkin <atkinchris@gmail.com>
2019-12-28 21:48:34 +0000

    Add input prompts for other players

--- src/index.ts
+++ src/index.ts
@@ -13,17 +13,21 @@ import {
 } from './domino'
 
 // const HAND_SIZE = 15
-// const HIGHEST_DOUBLE = 12
-// const NUMBER_OF_PLAYERS = 4
+const HIGHEST_DOUBLE = 6
+const NUMBER_OF_PLAYERS = 4
 // const STATIONS = 8
 
-let isFirstTurn = true
-let startingDomino: Domino
 let hand: Domino[]
-// const trains = []
+let startingDomino: Domino
+const chains: Domino[][] = []
 
 const takeTurn = async () => {
-  const allDominos = generateDominos(12)
+  const allDominos = generateDominos(HIGHEST_DOUBLE)
+
+  const getAvailableDominos = () => {
+    const allocatedDominos = [...chains, hand, [startingDomino]].filter(Boolean)
+    return allDominos.filter(domino => !allocatedDominos.some(chain => chain.find(d => isEqual(d, domino))))
+  }
 
   if (!startingDomino) {
     const { startingDominoInput } = await prompt<{ startingDominoInput: string }>([
@@ -43,22 +47,58 @@ const takeTurn = async () => {
         type: 'checkbox',
         name: 'handInput',
         message: 'Please select dominos in your hand',
-        choices: allDominos.filter(d => !isEqual(d, startingDomino)).map(toString),
+        choices: getAvailableDominos().map(toString),
       },
     ])
     hand = handInput.map(fromString)
   }
 
-  if (isFirstTurn) {
-    isFirstTurn = false
+  if (chains[0] === undefined) {
     const longestChain = getLongestChain(startingDomino, hand)
 
     if (longestChain) {
       console.log(chalk.bold('Starting chain:'))
       console.log(chalk.blue(formatDominos(longestChain)))
+      console.log(chalk.grey('Assigning chain to first slot'))
+
+      chains[0] = longestChain
     } else {
       console.log(chalk.grey('No possible starting chain'))
+      console.log(chalk.grey('Assigning empty chain to first slot'))
+
+      chains[0] = []
     }
+  }
+
+  for (let i = 2; i <= NUMBER_OF_PLAYERS; i += 1) {
+    if (chains[i - 1] !== undefined) continue
+
+    const { chainElementsInput } = await prompt<{ chainElementsInput: string[] }>([
+      {
+        type: 'checkbox',
+        name: 'chainElementsInput',
+        message: `Please select starting chain for player ${i}`,
+        choices: getAvailableDominos().map(toString),
+      },
+    ])
+    const chainElements = chainElementsInput.map(fromString)
+
+    if (chainElements.length === 0) {
+      chains[i - 1] = []
+      continue
+    }
+
+    const { lastDominoInput } = await prompt<{ lastDominoInput: string }>([
+      {
+        type: 'list',
+        name: 'lastDominoInput',
+        message: `Please select tail domino for player ${i}`,
+        choices: chainElements.map(toString),
+      },
+    ])
+    const lastDomino = fromString(lastDominoInput)
+
+    chains[i - 1] = chainElements.filter(c => isEqual(c, lastDomino)).concat([lastDomino])
   }
 }
 
